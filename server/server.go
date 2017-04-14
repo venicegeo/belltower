08dc6dba87d8f76b8b93abcc26a27c5e5093e316
mpg <mpg@flaxen.com>
2017-04-13 19:42:34 -0600

    old code, not working

--- server/server.go
+++ server/server.go
@@ -1,61 +1,55 @@
 package server
 
-import (
-	"log"
-
-	"github.com/venicegeo/belltower/orm"
-)
-
 func Server() error {
+	/*
+		model, err := orm.NewOrm()
+		if err != nil {
+			return err
+		}
 
-	model, err := orm.NewOrm()
-	if err != nil {
-		return err
-	}
+		//
+		// Usr
+		//
+		userAttrs := &orm.UserFieldsForCreate{
+			Name:      "alice",
+			Role:      orm.AdminRole,
+			IsEnabled: true,
+		}
+		userId, err := model.CreateUser(model.AdminID, userAttrs)
+		if err != nil {
+			return err
+		}
+		log.Printf("userid: %d", userId)
 
-	//
-	// Usr
-	//
-	userAttrs := &orm.UserFieldsForCreate{
-		Name:      "alice",
-		Role:      orm.AdminRole,
-		IsEnabled: true,
-	}
-	userId, err := model.CreateUser(model.AdminID, userAttrs)
-	if err != nil {
-		return err
-	}
-	log.Printf("userid: %d", userId)
+		//
+		// Feed
+		//
+		feedAttrs := &orm.FeedFieldsForCreate{
+			Name:      "randomfeed",
+			FeedType:  "RandomFeed",
+			IsEnabled: true,
+			IsPublic:  false,
+			Settings: map[string]interface{}{
+				"name":  "randomfeed2",
+				"limit": "3",
+				"sleep": "5",
+			},
+		}
+		feedId, err := model.CreateFeed(userId, feedAttrs)
+		if err != nil {
+			return err
+		}
+		log.Printf("feedid: %d", feedId)
 
-	//
-	// Feed
-	//
-	feedAttrs := &orm.FeedFieldsForCreate{
-		Name:      "randomfeed",
-		FeedType:  "RandomFeed",
-		IsEnabled: true,
-		IsPublic:  false,
-		Settings: map[string]interface{}{
-			"name":  "randomfeed2",
-			"limit": "3",
-			"sleep": "5",
-		},
-	}
-	feedId, err := model.CreateFeed(userId, feedAttrs)
-	if err != nil {
-		return err
-	}
-	log.Printf("feedid: %d", feedId)
+		//
+		// Action
+		//
 
-	//
-	// Action
-	//
+		//
+		// Rule
+		//
 
-	//
-	// Rule
-	//
-
-	// time.Sleep(10 * time.Second)
-
+		// time.Sleep(10 * time.Second)
+	*/
 	return nil
 }
